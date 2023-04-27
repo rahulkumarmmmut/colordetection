59b6a574acd7b6cd1050a7339214c65970ab6375
Rahul <rahulk2903@gmail.com>
2023-04-27 15:54:56 -0400

    update

--- app/src/main/java/com/example/colordetection/home/HomeActivity.kt
+++ app/src/main/java/com/example/colordetection/home/HomeActivity.kt
@@ -3,16 +3,18 @@ package com.example.colordetection.home
 import android.app.Activity
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.colordetection.R
 import com.github.dhaval2404.imagepicker.ImagePicker
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 
 class HomeActivity : AppCompatActivity() {
 
@@ -44,6 +46,7 @@ class HomeActivity : AppCompatActivity() {
             cameraButton.setOnClickListener {
                 ImagePicker.with(this)
                     .crop()
+
                     .maxResultSize(
                         1080,
                         1080
@@ -60,6 +63,8 @@ class HomeActivity : AppCompatActivity() {
         if (resultCode == Activity.RESULT_OK) {
             //Image Uri will not be null for RESULT_OK
             uri= data?.data!!
+            val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
+
             var imageView: ImageView = findViewById(R.id.imageView)
             // Use Uri object instead of File to avoid storage permissions
             imageView.setImageURI(uri)
